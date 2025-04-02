92bd63cd529da59114b9a52897cbbdeba45a7b2c
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2025-04-02 16:31:26 +0000

    feature: add jiujitsu graus

---
+++ database/seeders/GrauJitJitsu.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GrauJitJitsu extends Seeder
+{
+    public function run(): void
+    {
+        $graus = [
+            ['nome' => 'Branca', 'nivel' => 1, 'sistema_graduacao_id' => 1],
+            ['nome' => 'Amarela', 'nivel' => 2, 'sistema_graduacao_id' => 1],
+            ['nome' => 'Laranja', 'nivel' => 3, 'sistema_graduacao_id' => 1],
+            ['nome' => 'Verde', 'nivel' => 4, 'sistema_graduacao_id' => 1],
+            ['nome' => 'Azul', 'nivel' => 5, 'sistema_graduacao_id' => 1],
+            ['nome' => 'Roxa', 'nivel' => 6, 'sistema_graduacao_id' => 1],
+            ['nome' => 'Marrom', 'nivel' => 7, 'sistema_graduacao_id' => 1],
+            ['nome' => 'Preta', 'nivel' => 8, 'sistema_graduacao_id' => 1]
+        ];
+
+        DB::table('graus')->insert($graus);
+    }
+}
