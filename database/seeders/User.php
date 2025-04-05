9a2e3dd651191d173dab01c23d41ab64b7f8349a
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2025-04-05 12:49:50 -0300

    fix: User Seeder

--- database/seeders/User.php
+++ database/seeders/User.php
@@ -4,14 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Seeder
 {
     public function run(): void
     {
         $user = [
-            ['name' => 'Vitor Hugo (Dev)', 'password_reset_next_login' => true, 'password' => 'vi123', 'email' => 'vitor@gmail.com.br'],
-            ['name' => 'Elies Junior (Sensei)', 'password_reset_next_login' => true, 'password' => 'eli123', 'email' => 'elies@gmail.com.br'],
+            ['name' => 'Vitor Hugo (Dev)', 'password_reset_next_login' => true, 'password' => Hash::make('vitor123'), 'email' => 'vitor@gmail.com.br', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Elies Junior (Sensei)', 'password_reset_next_login' => true, 'password' => Hash::make('elies123'), 'email' => 'elies@gmail.com.br', 'created_at' => now(), 'updated_at' => now()],
         ];
 
         DB::table('users')->insert($user);
