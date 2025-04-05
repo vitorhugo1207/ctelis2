e71b144f142526cfac6a6b3fbedb8e2b65eccd26
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2025-04-05 12:22:37 -0300

    refractor: setting routes

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,9 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class)->name('home');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/', HomeController::class)->name('home');
+
     Route::get('dashboard', DashboardController::class)->middleware(['verified'])->name('dashboard');
 
     Route::controller(ProfileController::class)->group(function () {
