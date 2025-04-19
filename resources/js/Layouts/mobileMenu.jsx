09fed92f92fd7535b6c972d4eea69c54590bf471
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2025-04-19 14:57:02 -0300

    feature: doing alunos list

--- resources/js/Layouts/mobileMenu.jsx
+++ resources/js/Layouts/mobileMenu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SpeedDial } from 'primereact/speeddial';
 import { Inertia } from '@inertiajs/inertia';
+import { Tooltip } from 'primereact/tooltip';
 
 export default function MobileMenu({ className }) {
 
@@ -12,17 +13,29 @@ export default function MobileMenu({ className }) {
                 Inertia.post(route('logout'));
             }
         },
+        {
+            label: 'Alunos',
+            icon: 'pi pi-users',
+            command: () => {
+                Inertia.get(route('alunos.index'));
+            }
+        }
     ];
 
     return (
-        <div className={`z-40 fixed right-[4.8rem] top-[85vh] ${className}`}>
-            <SpeedDial model={items}
-                mask
-                showIcon="pi pi-bars"
-                hideIcon="pi pi-times"
-                transitionDelay={80}
-                radius={120}
-                direction="up" />
+        <div className={`z-40 fixed right-[4.8rem] top-[81vh] ${className}`}>
+            <div>
+                <SpeedDial model={items}
+                    mask
+                    buttonClassName="p-button-help"
+                    className='speeddial-up'
+                    showIcon="pi pi-bars"
+                    hideIcon="pi pi-times"
+                    transitionDelay={80}
+                    radius={120}
+                    direction="up" />
+                <Tooltip target=".speeddial-up .p-speeddial-action" position="left" />
+            </div>
         </div>
     )
 }
