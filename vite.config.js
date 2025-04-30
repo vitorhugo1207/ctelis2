4ab31a26bb004144a9fa424829f105b4a84200ac
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2025-04-30 02:18:41 -0300

    some corrections

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
         },
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8000', // Ensure this matches your backend URL
+                target: 'http://127.0.0.1:8000',
                 changeOrigin: true,
                 secure: false,
             },
